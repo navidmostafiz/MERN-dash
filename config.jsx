f3f5e79cd53354ec98dbc882ff8290f76570dbed
Navid Mostafiz <navidmostafiz@gmail.com>
2018-02-21 22:54:19 -0500

    Delete config.jsx

--- config.jsx
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-import toastr from 'toastr';
-
-axios.defaults.baseURL = process.env.API_URL;
-
-toastr.options.positionClass = 'toast-bottom-right';
-toastr.options.closeButton = true;
